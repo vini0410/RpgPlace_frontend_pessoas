7484d0ac8460c832fa60bf812b657caed492334f
vini <vini0420@gmail.com>
2023-09-22 00:09:23 -0300

    config mfe

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -16,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'RpgPlaceFrontendPessoas'`, () => {
+  it(`should have as title 'RpgPlace_frontend_pessoas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('RpgPlaceFrontendPessoas');
+    expect(app.title).toEqual('RpgPlace_frontend_pessoas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('RpgPlaceFrontendPessoas app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('RpgPlace_frontend_pessoas app is running!');
   });
 });
